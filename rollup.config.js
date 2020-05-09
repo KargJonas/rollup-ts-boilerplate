c2363293ba4fd92940f9b2e6f83e445749329f75
Jonas Karg <jonas.karg@hotmail.com>
2020-05-09 23:28:55 +0200

    Merged the plugins arrays in the boilerplate config

--- rollup.config.js
+++ rollup.config.js
@@ -9,42 +9,42 @@ const output = process.argv.includes("-w") ? [
     sourcemap: true
   }
 ] : [
-  {
-    file: "./dist/bundle.js",
-    format: "iife",
-    name: "lib"
-  },
-  {
-    file: "./dist/bundle.cjs.js",
-    format: "cjs",
-  },
-  {
-    file: "./dist/bundle.esm.js",
-    format: "esm",
-  },
-  {
-    file: "./example/lib.js",
-    format: "iife",
-    name: "lib",
-    sourcemap: true
-  }
-];
+    {
+      file: "./dist/bundle.js",
+      format: "iife",
+      name: "lib"
+    },
+    {
+      file: "./dist/bundle.cjs.js",
+      format: "cjs",
+    },
+    {
+      file: "./dist/bundle.esm.js",
+      format: "esm",
+    },
+    {
+      file: "./example/lib.js",
+      format: "iife",
+      name: "lib",
+      sourcemap: true
+    }
+  ];
 
 export default {
   input: "./src/index.ts",
   output,
 
-  plugins: [liveServer({
-    port: 5000,
-    host: "localhost",
-    root: "example",
-    file: "index.html",
-    mount: [],
-    open: false,
-    wait: 500
-  })],
-
   plugins: [
+    liveServer({
+      port: 5000,
+      host: "localhost",
+      root: "example",
+      file: "index.html",
+      mount: [],
+      open: false,
+      wait: 500
+    }),
+
     typescript({
       inlineSourceMap: true
     })
