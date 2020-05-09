bc5be994bb4ca16868b42f102dcbc9f7395edae4
Jonas Karg <jonas.karg@hotmail.com>
2020-05-09 22:52:24 +0200

    Basic WebGL setup

--- rollup.config.js
+++ rollup.config.js
@@ -1,27 +1,40 @@
 import typescript from "rollup-plugin-typescript2";
 import liveServer from "rollup-plugin-live-server";
 
-const dev = process.argv.includes("-w");
-
-const exampleOut = {
-  file: "./example/lib.js",
-  format: "iife",
-  name: "lib",
-  sourceMap: "inline"
-};
-
-const output = dev ? exampleOut : [
-  exampleOut,
-  { file: "./dist/bundle.js", format: "iife", name: "lib" },
-  { file: "./dist/bundle.cjs.js", format: "cjs", },
-  { file: "./dist/bundle.esm.js", format: "esm", },
+const output = process.argv.includes("-w") ? [
+  {
+    file: "./example/lib.js",
+    format: "iife",
+    name: "Plotter",
+    sourcemap: true
+  }
+] : [
+  {
+    file: "./dist/bundle.js",
+    format: "iife",
+    name: "lib"
+  },
+  {
+    file: "./dist/bundle.cjs.js",
+    format: "cjs",
+  },
+  {
+    file: "./dist/bundle.esm.js",
+    format: "esm",
+  },
+  {
+    file: "./example/lib.js",
+    format: "iife",
+    name: "lib",
+    sourcemap: true
+  }
 ];
 
 export default {
   input: "./src/index.ts",
   output,
 
-  plugins: [dev ? liveServer({
+  plugins: [liveServer({
     port: 5000,
     host: "localhost",
     root: "example",
@@ -29,9 +42,11 @@ export default {
     mount: [],
     open: false,
     wait: 500
-  }) : null],
+  })],
 
   plugins: [
-    typescript({sourceMap: true}),
+    typescript({
+      inlineSourceMap: true
+    })
   ]
 }
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,50 @@
-export default function sayFoo() {
-  console.log("foo");
-}
+export default class Plotter {
+  public canvas: HTMLCanvasElement;
+  public gl: WebGLRenderingContext;
 
-sayFoo();
+  // Geometry of the drawing surface
+  private vertexCode: string = 'attribute vec2 coordinates;void main(void){gl_Position=vec4(coordinates,0.0,1.0);}';
+  private vertices: number[] = [-0.5, 0.5, -0.5, -0.5, 0.0, -0.5];
+  private shaderProgram: WebGLProgram;
+  private vertexBuffer: WebGLBuffer;
+
+  constructor(canvas: HTMLCanvasElement) {
+    // Setting Canvas and creating a rendering context
+    this.canvas = canvas;
+    this.gl = canvas.getContext("webgl");
+    this.shaderProgram = this.gl.createProgram();
+
+    this.createBuffer();
+    this.compileShader(this.gl.VERTEX_SHADER, this.vertexCode);
+  }
+
+  private createBuffer() {
+    this.vertexBuffer = this.gl.createBuffer();
+    this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.vertexBuffer);
+    this.gl.bufferData(this.gl.ARRAY_BUFFER, new Float32Array(this.vertices), this.gl.STATIC_DRAW);
+    this.gl.bindBuffer(this.gl.ARRAY_BUFFER, null);
+  }
+
+  private compileShader(type: number, code: string) {
+    const shader: WebGLShader = this.gl.createShader(type);
+    this.gl.shaderSource(shader, code);
+    this.gl.compileShader(shader);
+    this.gl.attachShader(this.shaderProgram, shader);
+  }
+
+  private run() {
+    this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.vertexBuffer);
+    this.gl.linkProgram(this.shaderProgram);
+    this.gl.useProgram(this.shaderProgram);
+
+    const coord = this.gl.getAttribLocation(this.shaderProgram, "coordinates");
+    this.gl.vertexAttribPointer(coord, 2, this.gl.FLOAT, false, 0, 0);
+    this.gl.enableVertexAttribArray(coord);
+
+    this.gl.clearColor(0.5, 0.5, 0.5, 0.9);
+    this.gl.enable(this.gl.DEPTH_TEST);
+    this.gl.clear(this.gl.COLOR_BUFFER_BIT);
+    this.gl.viewport(0, 0, this.canvas.width, this.canvas.height);
+    this.gl.drawArrays(this.gl.TRIANGLES, 0, 3);
+  }
+}
